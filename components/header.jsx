95047e9eab339bb1eac50b7119d7848db170400e
BTH-M3ND3S <mendessilveriomateus@gmail.com>
2024-01-25 20:10:20 -0300

    protótipo header

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,22 @@
-function header(){
+import React from 'react';
+import Style from '../css/header.module.css';
 
-    return(
-        <>
+function Header() {
+  return (
+    <div className={Style.header}>
+      <div className={Style.divlogo}>
+        <h3>logo</h3>
+      </div>
+      <div className={Style.divpesquisa}>
+        <input style={{width: '65%', height: '35%'}} type="search" name="pesquisa" id="pesquisa" />
         
-        </>
-
-    )
-
+      </div>
+      <div className={Style.divusuario}>
+        <button>Cadastrar</button>
+        <button>Entrar</button>
+      </div>
+    </div>
+  );
 }
-export default header;
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import Style from '../css/index.module.css'
+import Header from "../components/header";
 function home(){
     return(
-        <>
-        </>
+    <div className={Style.Container}>
+      <Header/>
+    </div>
     )
 }
 export default home;
